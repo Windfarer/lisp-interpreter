bd8a73405c3aac4c7db6cca92e04948da34272f5
Windfarer <windfarer@gmail.com>
2017-11-26 00:52:48 +0800

    cleaning

--- printer.py
+++ printer.py
@@ -2,11 +2,7 @@ from mal_types import MalType, MalSymbol, MalNumber, MalList
 
 
 def pr_str(obj):
-    if isinstance(obj, MalSymbol):
-        return str(obj.value)
-    elif isinstance(obj, MalNumber):
-        return str(obj.value)
-    elif isinstance(obj, MalList):
+    if isinstance(obj, MalList):
         return obj.p_type[0] + " ".join([pr_str(i) for i in obj]) + obj.p_type[1]
     else:
         return str(obj)
